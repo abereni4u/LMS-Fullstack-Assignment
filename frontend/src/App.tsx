e4a666965c57b68ea4dc7b1c3eb2670d365c794e
abereni4u <aberenijopuiyo@gmail.com>
2026-06-05 01:50:35 -0400

    Add routes to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 import {Routes, Route} from 'react-router'
 import './App.css'
-import Login from './components/Login'
+import Login from './components/Login.tsx'
+import Create from './components/Create.tsx'
+import Edit from './components/Edit.tsx'
+import Delete from './components/Delete.tsx'
+
 
 function App() {
   return (
@@ -9,6 +13,9 @@ function App() {
 
       <Routes>
           <Route path="" element={<Login/>}/>
+          <Route path="/edit:id" element={<Edit/>}/>
+          <Route path="/create" element={<Create/>}/>
+          <Route path="/delete:id" element={<Delete/>}/>
       </Routes>
 
    </>
