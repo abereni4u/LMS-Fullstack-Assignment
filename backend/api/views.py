a278ec50b38b3b893873d2b91be26175fd5aad8e
abereni4u <aberenijopuiyo@gmail.com>
2026-06-05 16:00:04 -0400

    Add querying based off student ID to EnrollmentViewset

--- backend/api/views.py
+++ backend/api/views.py
@@ -31,6 +31,9 @@ class EnrollmentViewset(viewsets.ModelViewSet):
     queryset = Enrollment.objects.all()
     serializer_class = EnrollmentSerializer
 
+    def get_queryset(self):
+        return Enrollment.objects.filter(student=self.request.user)
+
     def perform_create(self, serializer):
         serializer.save(student=self.request.user)
 
