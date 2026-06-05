9175faa852c86a44eecca76799de732db3b3b275
abereni4u <aberenijopuiyo@gmail.com>
2026-06-05 14:03:16 -0400

    Add models to admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Course, Chapter, Enrollment
 
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Chapter)
+admin.site.register(Enrollment)
 # Register your models here.
